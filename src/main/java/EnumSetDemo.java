8c169a630431bc9cb9bd7e2171ece0211ecd7bea
balajeev <Balajee.Venkatesh@bitwiseglobal.com>
2016-02-25 17:20:33 +0530

    Initial commit

---
+++ src/main/java/EnumSetDemo.java
@@ -0,0 +1,43 @@
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class EnumSetDemo {
+
+    private enum Color {
+
+        RED(255, 0, 0), GREEN(0, 255, 0), BLUE(0, 0, 255);
+        private int r;
+        private int g;
+        private int b;
+        private Color(int r, int g, int b)
+
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+        EnumSet<Color> yellow = EnumSet.of(Color.RED, Color.GREEN);
+        drawLine(yellow);
+
+        EnumSet<Color> white = EnumSet.of(Color.RED, Color.GREEN, Color.BLUE);
+        drawLine(white);
+
+        EnumSet<Color> pink = EnumSet.of(Color.RED, Color.BLUE);
+        drawLine(pink);
+    }
+
+
+    public static void drawLine(Set<Color> colors) {
+        System.out.println("Requested Colors to draw lines : " + colors);
+        for (Color c : colors) {
+            System.out.println("drawing line in color : " + c);
+        }
+    }
+}
